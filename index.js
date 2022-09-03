eb49c2334ae3e4f800ab1fdeab512c0f7dc40a57
Michael <mikahado@gmail.com>
2022-09-02 23:24:37 -0400

    solved football lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+
+
+function superbowlWin(record){
+    const win = record.find(({result}) => result === 'W');
+            if(win) {
+            return win.year;
+            }
+        }
+
+        
+
+
